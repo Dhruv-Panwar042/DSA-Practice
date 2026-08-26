aadad104d0af7223a0a7ebd02f9c77da0e0d3c0f
Dhruv-Panwar042 <dhruvspanwar12@gmail.com>
2026-08-26 23:37:08 +0530

    26th August

---
+++ LeetCode/Recurssion/46.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void recurrence(int index, vector<int>& nums, vector<vector<int>>& ans) {
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = index; i < nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            recurrence(index + 1, nums, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recurrence(0, nums, ans);
+        return ans;
+    }
+};
